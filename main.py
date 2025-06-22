588e5226d6942c0c226e885169cc0385faab6484
juozasg <juozasgaigalas@gmail.com>
2025-06-22 23:08:12 +0300

    pygame is the way

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+import sys
 import pygame
 from pygame.locals import *
 
-import hello
+Clock = pygame.time.Clock
 
-pygame.init()
+# Predefined some colors
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+def main():
+    pygame.init()
+    disp = pygame.display.set_mode((1920, 1080))
+    disp.fill(WHITE)
+    pygame.display.set_caption('Hello Arp Souls')
+
+    # running = True
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+        ms = Clock().tick_busy_loop(60)
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/hello.py
@@ -0,0 +1,48 @@
+import pygame
+from pygame.locals import *
+import random
+
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+class Enemy(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center=(random.randint(40,SCREEN_WIDTH-40),0)
+
+      def move(self):
+        self.rect.move_ip(0,10)
+        if (self.rect.bottom > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+
+      def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+       #if pressed_keys[K_UP]:
+            #self.rect.move_ip(0, -5)
+       #if pressed_keys[K_DOWN]:
+            #self.rect.move_ip(0,5)
+
+        if self.rect.left > 0:
+              if pressed_keys[K_LEFT]:
+                  self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+              if pressed_keys[K_RIGHT]:
+                  self.rect.move_ip(5, 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
