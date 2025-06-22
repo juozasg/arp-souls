19ac07f3efcaba9917080640ee8a9c08968348cc
juozasg <juozasgaigalas@gmail.com>
2025-06-23 01:10:44 +0300

    arcade now

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
+import os
+from pathlib import Path
 import pyglet
 import arcade
 
 from constants import WINDOW_HEIGHT, WINDOW_WIDTH
+from gameview import GameView
+
+asset_dir = os.path.join(Path(__file__).parent.resolve(), "assets")
+arcade.resources.add_resource_handle("assets", asset_dir)
 
 
-class GameView(arcade.Window):
-    def __init__(self):
-        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, "Hello Arp Souls")
-        self.background_color = arcade.color.WHITE
 
-    def setup(self):
-        pass
 
-    def center_on_screen(self):
-        viewport = pyglet.display.get_display().get_default_screen()
-        left = (viewport.width - WINDOW_WIDTH) // 4
-        top = (viewport.height - WINDOW_HEIGHT) // 4
-        self.set_location(left, top)
-
-    def on_draw(self):
-        self.clear()
-        # arcade.start_render()
-        # arcade.draw_text("Hello Arp Souls", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
-        #                  arcade.color.BLACK, font_size=24, anchor_x="center")
+def center_window(window):
+    viewport = pyglet.display.get_display().get_default_screen()
+    left = (viewport.width - WINDOW_WIDTH) // 4
+    top = (viewport.height - WINDOW_HEIGHT) // 4
+    window.set_location(left, top)
 
 def main():
-    window = GameView()
-    window.setup()
-    window.center_on_screen()
+    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Hello Arp Souls")
+    game = GameView()
+    window.show_view(game)
+    center_window(window)
+
     arcade.run()
 
 
---
+++ src/gameview.py
@@ -0,0 +1,47 @@
+import arcade
+
+from constants import TILE_SCALING
+
+class GameView(arcade.View):
+    def __init__(self):
+        super().__init__()
+
+        self.player_texture = arcade.load_texture(":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png")
+        self.player_sprite = arcade.Sprite(self.player_texture)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", scale=TILE_SCALING)
+            wall.position = coordinate
+            self.wall_list.append(wall)
+
+        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+
+
+
+        self.background_color = arcade.color.WHITE
+
+    def setup(self):
+        pass
+
+
+    def on_draw(self):
+        self.clear()
+        # Code to draw other things will go here
+        arcade.draw_sprite(self.player_sprite)
+        self.wall_list.draw()
+
+        # arcade.start_render()
+        # arcade.draw_text("Hello Arp Souls", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+        #                  arcade.color.BLACK, font_size=24, anchor_x="center")
