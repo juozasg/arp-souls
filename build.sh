aa2467267027d5ae66190d8b769b1a185886a59d
juozasg <juozasgaigalas@gmail.com>
2025-06-23 18:28:14 +0300

    building

---
+++ build.sh
@@ -0,0 +1,2 @@
+python -m nuitka 'main.py' --follow-imports --include-data-dir='C:\Users\juozas\Code\arp-souls\assets'='.\assets' --include-package=arcade.gl.backends.opengl --include-package=mido.backends.rtmidi --standalone
+python -m nuitka 'main.py' --follow-imports --include-data-dir='C:\Users\juozas\Code\arp-souls\assets'='.\assets' --include-package=arcade.gl.backends.opengl --include-package=mido.backends.rtmidi --onefile
