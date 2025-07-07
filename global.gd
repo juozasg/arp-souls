12311b8f287e511074d23090c263d2f9a427cfda
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-07-07 18:10:50 +0300

    ui complete

--- global.gd
+++ global.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const res_x = 640
+const res_y = 360
 func _ready() -> void:
 	print("AUTOLOAD")
 	print(DisplayServer.screen_get_size())
@@ -9,7 +11,7 @@ func _ready() -> void:
 	if JavaScriptBridge.get_interface('window') != null:
 		scale_factor = clamp(scale_factor, 1, 3)
 
-	get_window().content_scale_size = Vector2i(640, 360)
-	get_window().size = Vector2i(640 * scale_factor, 360 * scale_factor)
+	get_window().content_scale_size = Vector2i(res_x, res_y)
+	get_window().size = Vector2i(res_x * scale_factor, res_y * scale_factor)
 	get_window().position = Vector2i(DisplayServer.screen_get_size().x/2, 300 )
 	#get_window().type
