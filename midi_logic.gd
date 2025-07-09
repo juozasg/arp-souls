6245eaf4c27c6c7dcec2e4c611ec00c1a39d332c
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-07-10 01:37:59 +0300

    bad math

--- midi_logic.gd
+++ midi_logic.gd
@@ -1,8 +1,9 @@
 extends Node
 
-var times: Array[float] = []
-var dtimes: Array[float] = []
+var ticks: Array[float] = []
+var dticks: Array[float] = []
 var tick0 = Time.get_ticks_msec()
+var bpm = 0.0
 
 func _ready():
 	OS.open_midi_inputs()
@@ -21,15 +22,34 @@ func _input(input_event):
 		%PianoPlayer.note_event(midi_event)
 		
 	if midi_event.message == MIDI_MESSAGE_NOTE_ON:
-		var tick = Time.get_ticks_msec() - tick0
-		times.append(tick)
-		if times.size() > 4:
-			times = times.slice(1, 5)
-		dtimes.clear()
-		for i in range(0, times.size() - 1):
-			dtimes.append(times[i+1] - times[1])
-		print('dtick ' , tick)
+		calc_tempo()
 
+		
+		
+
+func calc_tempo():
+	var tick = Time.get_ticks_msec() - tick0
+	if ticks.size() > 0:
+		var lastdt = tick - ticks[-1]
+		if(lastdt > 1000):
+			ticks.clear()
+	ticks.append(tick)
+	if ticks.size() > 5:
+		ticks = ticks.slice(1, 6)
+	dticks.clear()
+	for i in range(0, ticks.size() - 1):
+		dticks.append(ticks[i+1] - ticks[i])
+	#print('dtick ' , tick)
+	%DebugLabel.text = "TS: %s\nDTS: %s   BPM: %.1f" % [ticks, dticks, bpm]
+	
+	if dticks.size() < 2:
+		%RHYTHM.text = "X"
+	else:
+		var mean = dticks.reduce(func(t, sum): return sum + t) / dticks.size()
+		bpm = 60_000 / mean
+		var variance = dticks.reduce(func(t, sum): return (t - mean) ** 2) / (dticks.size())
+		var stdev = (variance ** 0.5) / 100
+		%RHYTHM.text = "%.1f" % stdev
 
 
 #func _physics_process(delta: float) -> void:
